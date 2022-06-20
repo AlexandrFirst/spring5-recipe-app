219986dbffa6b05e2e4789b5760a4285d519cbc4
Alex <50964369+AlexandrFirst@users.noreply.github.com>
2022-06-20 16:34:37 +0300

    recepi app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO category (category_name) VALUES ('American'), ('Italian'), ('Mexican'), ('Fast food');
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon'), ('Tablespoon'), ('Cup'), ('Pinch'), ('Ounce'), ('Each'), ('Dash'), ('Pint');
